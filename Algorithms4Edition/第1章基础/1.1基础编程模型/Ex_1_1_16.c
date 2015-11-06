4d7667cb77d43206825221927a2759f5b673755c
zykhbl <zykhbl@126.com>
2015-11-06 22:36:43 +0800

    修改代码

--- Algorithms4Edition/第1章基础/1.1基础编程模型/Ex_1_1_16.c
+++ Algorithms4Edition/第1章基础/1.1基础编程模型/Ex_1_1_16.c
@@ -10,6 +10,105 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct string {
+    char *s;
+    size_t len;
+};
+
+struct string* itostr_10(int n) {
+    struct string *str = malloc(sizeof(struct string));
+    str->s = malloc(10 * sizeof(char));
+    
+    int i = 0;
+    if (n == 0) {
+        *(str->s + i) = '0';
+        i++;
+    } else {
+        for (; n > 0; i++, n /= 10) {
+            *(str->s + i) = '0' + n % 10;
+        }
+    }
+    
+    *(str->s + i) = '\0';
+    str->len = i;
+    
+    for (int j = 0; j < i / 2; j++) {
+        char t = *(str->s + j);
+        *(str->s + j) = *(str->s + i - 1 - j);
+        *(str->s + i - 1 - j) = t;
+    }
+    
+    return str;
+}
+
+struct string * ex_1_1_16(int n) {
+    static struct string **itoa = NULL;
+    static struct string **ff = NULL;
+    if (itoa == NULL) {
+        itoa = malloc((n + 1) * sizeof(struct string*));
+        memset(itoa, 0, (n + 1) * sizeof(struct string*));
+    }
+    if (ff == NULL) {
+        ff = malloc((n + 1) * sizeof(struct string*));
+        memset(ff, 0, (n + 1) * sizeof(struct string*));
+    }
+    
+    if (n <= 0) return NULL;
+    
+    if (*(ff + n - 3) == NULL) {
+        *(ff + n - 3) = ex_1_1_16(n - 3);
+    }
+    struct string *s1 = *(ff + n - 3);
+    
+    if (*(ff + n - 2) == NULL) {
+        *(ff + n - 2) = ex_1_1_16(n - 2);
+    }
+    struct string *s2 = *(ff + n - 2);
+    
+    if (*(itoa + n) == NULL) {
+        *(itoa + n) = itostr_10(n);
+    }
+    struct string *s3 = *(itoa + n);
+    
+    size_t size = 2 * s3->len;
+    
+    if (s1 != NULL && s1->s != NULL) {
+        size += s1->len;
+    }
+    
+    if (s2 != NULL && s2->s != NULL) {
+        size += s2->len;
+    }
+    
+    struct string *str = malloc(sizeof(struct string));
+    str->s = malloc(size + 1);
+    str->len = size;
+    
+    int offset = 0;
+    if (s1 != NULL && s1->s != NULL) {
+        strcpy(str->s + offset, s1->s);
+        offset += s1->len;
+    }
+    
+    strcpy(str->s + offset, s3->s);
+    offset += s3->len;
+    
+    if (s2 != NULL && s2->s != NULL) {
+        strcpy(str->s + offset, s2->s);
+        offset += s2->len;
+    }
+    
+    strcpy(str->s + offset, s3->s);
+    
+    *(str->s + str->len) = '\0';
+    
+    if (*(ff + n) == NULL) {
+        *(ff + n) = str;
+    }
+    
+    return str;
+}
+
 char* itoa_10(int n) {
     char *s = malloc(10 * sizeof(char));
     
@@ -34,9 +133,10 @@ char* itoa_10(int n) {
     return s;
 }
 
-char* ex_1_1_16(int n) {
+char* ex_1_1_16_fast(int n) {
     static char **itoa = NULL;
     static char **ff = NULL;
+    
     if (itoa == NULL) {
         itoa = malloc((n + 1) * sizeof(char*));
     }
@@ -47,12 +147,12 @@ char* ex_1_1_16(int n) {
     if (n <= 0) return NULL;
     
     if (*(ff + n - 3) == NULL) {
-        *(ff + n - 3) = ex_1_1_16(n - 3);
+        *(ff + n - 3) = ex_1_1_16_fast(n - 3);
     }
     char *s1 = *(ff + n - 3);
     
     if (*(ff + n - 2) == NULL) {
-        *(ff + n - 2) = ex_1_1_16(n - 2);
+        *(ff + n - 2) = ex_1_1_16_fast(n - 2);
     }
     char *s2 = *(ff + n - 2);
     
@@ -61,7 +161,7 @@ char* ex_1_1_16(int n) {
     }
     char *s3 = *(itoa + n);
     
-    size_t size = 2 * strlen(s3) + 1;
+    size_t size = 2 * strlen(s3);
     
     if (s1 != NULL) {
         size += strlen(s1);
@@ -71,7 +171,7 @@ char* ex_1_1_16(int n) {
         size += strlen(s2);
     }
     
-    char *s = malloc(size);
+    char *s = malloc(size + 1);
     
     int offset = 0;
     if (s1 != NULL) {
@@ -89,7 +189,7 @@ char* ex_1_1_16(int n) {
     
     strcpy(s + offset, s3);
     
-    *(s + (size - 1)) = '\0';
+    *(s + size) = '\0';
     
     if (*(ff + n) == NULL) {
         *(ff + n) = s;
@@ -105,7 +205,7 @@ char* ex_1_1_16_slow(int n) {
     char *s2 = ex_1_1_16_slow(n - 2);
     char *s3 = itoa_10(n);
     
-    size_t size = 2 * strlen(s3) + 1;
+    size_t size = 2 * strlen(s3);
     
     if (s1 != NULL) {
         size += strlen(s1);
@@ -115,7 +215,7 @@ char* ex_1_1_16_slow(int n) {
         size += strlen(s2);
     }
     
-    char *s = malloc(size);
+    char *s = malloc(size + 1);
     
     int offset = 0;
     if (s1 != NULL) {
@@ -133,7 +233,7 @@ char* ex_1_1_16_slow(int n) {
     
     strcpy(s + offset, s3);
     
-    *(s + (size - 1)) = '\0';
+    *(s + size) = '\0';
     
     if (s1 != NULL) {
         free(s1);
@@ -149,9 +249,14 @@ char* ex_1_1_16_slow(int n) {
 }
 
 int test_ex_1_1_16(int argc, const char * argv[]) {
-    char *s = ex_1_1_16(36);
-    printf("%s\n", s);
-    free(s);
+//    char *s = ex_1_1_16_fast(71);
+//    printf("%s\n", s);
+//    free(s);
+    
+    struct string *str = ex_1_1_16(6);
+    printf("%s\n", str->s);
+    free(str->s);
+    free(str);
     
     return 0;
 }
